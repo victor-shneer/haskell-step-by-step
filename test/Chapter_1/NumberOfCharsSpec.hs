87e3fa644f179a190ad8be72743d50ed08157685
Victor Shneer <106147096+vshneer@users.noreply.github.com>
2022-09-07 21:29:51 +0300

    better test annotations

--- test/Chapter_1/NumberOfCharsSpec.hs
+++ test/Chapter_1/NumberOfCharsSpec.hs
@@ -6,7 +6,7 @@ import SpecHelper
 spec :: Spec
 spec =
   describe "charCount" $ do
-    context "123" $
+    context "with \"123\"" $
       it "should be 3\n" $
         charCount "123" `shouldBe` "3\n"
 
--- test/Chapter_1/NumberOfWordsSpec.hs
+++ test/Chapter_1/NumberOfWordsSpec.hs
@@ -5,7 +5,7 @@ import SpecHelper
 spec :: Spec
 spec =
   describe "wordCount" $ do
-    context "with first second third" $
+    context "with \"first second third\"" $
       it "should be 3\n" $
         wordCount "first second third" `shouldBe` "3\n"
 
